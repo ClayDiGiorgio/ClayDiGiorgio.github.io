65427f1ceb7425da907679370b073346d0aa7b3d
Clay DiGiorgio <cldigiorgio@knights.ucf.edu>
2020-04-24 13:23:05 -0400

    removed call to nonexistent function

--- Testing/visualizer/loadJSON.js
+++ Testing/visualizer/loadJSON.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function() {
       if (this.files && this.files[0]) {
           var img = document.querySelector('img');  // $('img')[0]
           img.src = URL.createObjectURL(this.files[0]); // set src to blob url
-          img.onload = imageIsLoaded;
+          //img.onload = imageIsLoaded;
           var json = loadJSONfromImage(img);
           console.log("json read:");
           console.log(json);
