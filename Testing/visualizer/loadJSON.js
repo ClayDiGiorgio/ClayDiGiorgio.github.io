33493353332eba3856f375b43a6c855576ea6144
Clay DiGiorgio <cldigiorgio@knights.ucf.edu>
2020-04-24 13:29:31 -0400

    testing width and height

--- Testing/visualizer/loadJSON.js
+++ Testing/visualizer/loadJSON.js
@@ -5,7 +5,12 @@ window.addEventListener('load', function() {
       if (this.files && this.files[0]) {
           var img = document.querySelector('img');  // $('img')[0]
           img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+          
           //img.onload = imageIsLoaded;
+          console.log("image width and height");
+          console.log(img.width);
+          console.log(img.height);
+          
           var json = loadJSONfromImage(img);
           console.log("json read:");
           console.log(json);
