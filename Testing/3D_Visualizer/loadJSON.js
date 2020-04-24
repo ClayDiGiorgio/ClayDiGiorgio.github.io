ddfe4170c60e3da0f43d4f2a4c26b0274855a845
Clay DiGiorgio <cldigiorgio@knights.ucf.edu>
2020-04-24 13:12:08 -0400

    added log messages

--- Testing/3D_Visualizer/loadJSON.js
+++ Testing/3D_Visualizer/loadJSON.js
@@ -1,11 +1,14 @@
 // below function modified from clabe45 via https://stackoverflow.com/a/45931408
 window.addEventListener('load', function() {
   document.querySelector('input[type="file"]').addEventListener('change', function() {
+      console.log("loading image...");
       if (this.files && this.files[0]) {
           var img = document.querySelector('img');  // $('img')[0]
           img.src = URL.createObjectURL(this.files[0]); // set src to blob url
           img.onload = imageIsLoaded;
           var json = loadJSONfromImage(img);
+          console.log("json read:");
+          console.log(json);
       }
   });
 });
