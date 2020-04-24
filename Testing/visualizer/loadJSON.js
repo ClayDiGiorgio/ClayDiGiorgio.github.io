c31aad5ba29ad29d492b5844953002935eae29ed
Clay DiGiorgio <cldigiorgio@knights.ucf.edu>
2020-04-24 17:23:31 -0400

    re-added previews for the image you upload

--- Testing/visualizer/loadJSON.js
+++ Testing/visualizer/loadJSON.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function() {
   document.querySelector('input[type="file"]').addEventListener('change', function() {
       console.log("loading image...");
       if (this.files && this.files[0]) {
-          var img = new Image(); //document.querySelector('img');  // $('img')[0]
+          var img = document.querySelector('img');  // $('img')[0]
           img.src = URL.createObjectURL(this.files[0]); // set src to blob url
           img.onload = loadJSONfromImage;
           
